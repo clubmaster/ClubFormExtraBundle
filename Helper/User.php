3aa7e70318656e70299aca7db57f2e248d9fa05d
Michael Holm <hollo@hollo.dk>
2014-05-13 01:02:35 +0200

    Added user password encoder.

---
+++ Helper/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Club\FormExtraBundle\Helper;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User
+{
+    private $container;
+
+    public function __construct($container
+    {
+        $this->container = $container;
+    }
+
+    public function encodePassword(UserInterface $user, $password)
+    {
+        $factory = $this->container->get('security.encoder_factory');
+        $encoder = $factory->getEncoder($user);
+
+        return $encoder->encodePassword($password, $user->getSalt());
+    }
+}
