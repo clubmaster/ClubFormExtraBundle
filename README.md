0fb67abdac6c2a6acb340cbf0715d3e06ef0972e
Michael Holm <hollo@hollo.dk>
2014-06-16 12:45:05 +0200

    Added slideshow.

--- README.md
+++ README.md
@@ -19,3 +19,16 @@ twig:
 Remember if you use the tinymce form, you will properly want to disble form validation:
 
 <form novalidate="true">
+
+Added html slider:
+
+<ul id="slideshow">
+    <li class="active">Demo 1</li>
+    <li>Demo 2</li>
+    <li>Demo 3</li>
+</ul>
+
+<link href="bundles/clubformextra/css/slideshow.css" />
+<script src="bundles/clubformextra/js/slideshow.css"></script>
+
+That will add a slideshow for you.
---
+++ Resources/public/js/slideshow.js
@@ -0,0 +1,26 @@
+function slideSwitch() {
+    var $active = $('#slideshow li.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow li:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow li:first');
+
+    //$active.addClass('last-active');
+    $active.css({opacity: 1.0})
+        .animate({opacity: 0.0}, 1000, function() {
+            $active.addClass('last-active');
+        });
+
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(document).ready(function() {
+    setInterval( "slideSwitch()", 10000 );
+});
+
