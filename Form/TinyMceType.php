7f067ec8a7ed1854a4a534006205aba27b474a1d
Michael Holm <hollo@hollo.dk>
2014-12-26 18:22:39 +0100

    Added support for multiple tinymce instances.

--- Form/TinyMceType.php
+++ Form/TinyMceType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\FormInterface;
 class TinyMceType extends AbstractType
 {
     protected $container;
+    protected $instances = 0;
 
     public function __construct($container)
     {
@@ -24,6 +25,9 @@ class TinyMceType extends AbstractType
 
     public function finishView(FormView $view, FormInterface $form, array $options)
     {
+        $this->instances++;
+        $view->vars['tinymce_instances'] = $this->instances;
+
         if ($form->getConfig()->getAttribute('plugins')) {
             $plugins = $form->getConfig()->getAttribute('plugins');
             $view->vars['plugins'] = $plugins;
